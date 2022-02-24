c7a80b3de6156760d0e0069be0c7ebf7cd8e15b7
Mario Junior <mariojunior990@outlook.com>
2022-02-24 11:18:00 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/cursojava/curso/entities/User.java
+++ src/main/java/com/cursojava/curso/entities/User.java
@@ -1,13 +1,20 @@
 package com.cursojava.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -15,6 +22,11 @@ public class User implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name, email, phone, password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	
 	public User() {
 	}
@@ -67,6 +79,7 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
 
 	@Override
 	public int hashCode() {
@@ -76,6 +89,9 @@ public class User implements Serializable{
 		return result;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -92,6 +108,8 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
+
+	
 
 	
 	
---
+++ src/main/java/com/cursojava/curso/repositories/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.cursojava.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursojava.curso.entities.Order;
+
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+}
