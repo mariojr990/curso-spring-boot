93acfc3370076ff06dff4d4f5eb544b2e8edfa08
Mario Junior <mariojunior990@outlook.com>
2022-03-10 10:39:34 -0300

    Implementação das classes Category e Product

--- src/main/java/com/cursojava/curso/entities/Product.java
+++ src/main/java/com/cursojava/curso/entities/Product.java
@@ -11,27 +11,33 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
-@Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
-	private static final long serialVersionUID = 1L;
 
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable{
+
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
-
-	// Associação entre Categoria e Produto
+	private  String name, description, imgUrl;
+	private Double price;
+	
+	//Associação entre Produto e Categoria
 	@Transient
-	private Set<Product> products = new HashSet<>();
-
-	public Category() {
+	private Set<Category> categories = new HashSet<>();
+	
+	public Product() {
 	}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+		
 	}
 
 	public Long getId() {
@@ -50,8 +56,32 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
-	public Set<Product> getProducts() {
-		return products;
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
@@ -70,7 +100,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -78,5 +108,6 @@ public class Category implements Serializable {
 			return false;
 		return true;
 	}
-
+	
+	
 }
