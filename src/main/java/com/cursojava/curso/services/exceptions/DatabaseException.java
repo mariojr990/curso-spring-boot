b91110b3e8a455134a225096140d5567ae4bf8af
Mario Junior <mariojunior990@outlook.com>
2022-03-28 13:58:44 -0300

    Tratamento exceção Delete

---
+++ src/main/java/com/cursojava/curso/services/exceptions/DatabaseException.java
@@ -0,0 +1,14 @@
+package com.cursojava.curso.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+
+}
